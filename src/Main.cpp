72bc5d6d1cdd9855b4da6f5b96b7dae72b65bf44
Zimo <zimoLiao@mail.ustc.edu.cn>
2021-12-27 18:30:00 +0800

    update

---
+++ src/Main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	/* Pre-processing */
+	// Configuration parameters
+	const int nx = 100, ny = 100;
+
+	// Allocation
+	double ux=100
+
+
+
+
+}
